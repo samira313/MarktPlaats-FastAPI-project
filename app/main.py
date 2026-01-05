d1f4d0c59fd47e26b0a767eaa69eaa7b9775d6a6
Eslam <eslam.mohamed@motopp.nl>
2026-01-05 12:33:38 +0100

    refactor: use shared database module

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import app.core.config
 from fastapi import FastAPI
 from app.routers.ads import router as ads_router
+
 from app.routers.register import router as register_router
 from app.routers.login import router as login_router
+
 from app.db.database import Base, engine
 from app.models.ads import Ad
 from app.models import user
