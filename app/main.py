ccaad74961f4284dc82c7df630e137f3f68e095e
Erginmotopp <ergin.gungor@motopp.nl>
2026-01-06 13:14:45 +0100

    main.py changes

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.routers.ads import router as ads_router
 
 from app.routers.register import router as register_router
 from app.routers.login import router as login_router
-from app.routers import  ad_search
+
 from app.db.database import Base, engine
 from app.models.ads import Ad
 from app.models import user
